cba4a333b580f882c07e8e199989136cd7d3a7db
armanda09 <firminoamanda579@gmail.com>
2025-09-10 14:47:12 -0300

    Add files via upload
    Entrega da atividade calculo de imc por Amanda.

---
+++ Modulo 1/Codigos Basicos/calculo_de_imc.py
@@ -0,0 +1,6 @@
+Nome_do_paciente=input("digite um nome:")
+Peso=float(input("digite um peso:"))
+altura=float(input("digite a altura em metros:"))
+
+imc=Peso/(altura*altura)
+print("o seu indice de peso e:")
